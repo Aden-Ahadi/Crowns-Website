2ef059a1eaedcbebe1ef08076acf5b1b5161e199
Aden. <adenahadi@gmail.com>
2024-07-19 11:38:29 +0300

    This is the Entire comit

---
+++ JavaScript/orders.js
@@ -0,0 +1,23 @@
+function showalert() {
+  alert("Your order has been packed with lots of love ❤️.\n\n Arriving SOON!!");
+}
+
+function burger() {
+  alert("Ohh!! Burgers 😂. Nice choice.\n\n. Be there in a sec.🛫");
+}
+
+function swahili() {
+  alert("Mtu wa maana sana wewe 😁.\n\n Enjoy the swahili flavors.");
+}
+
+function makange() {
+  alert("Nothing like Makange for lunch.\n\n On our way!!🏍️");
+}
+
+function family() {
+  alert("Family is Everything 💕.\n\n Delivered in 2mins");
+}
+
+function kisinia() {
+  alert("Wanangu wa kisinia 🔥.\n\n Dakika 5 tu 🛺");
+}
